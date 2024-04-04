7156d8e52eef9355b3c8acf41eacb109afffa16e
byeun <sdfd23@>
2024-04-04 01:31:19 -0400

    added entity superclass and main runner

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Pew Pew Adventures");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack();
+
+        window.setLocation(0, 0);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
